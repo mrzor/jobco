5d1065fc65375bb11da2b725ad543a52ed9dc5c0
mrzor <eli@mrzor.com>
2011-11-01 22:29:29 +0100

    use marshalling instead of JSON for configuration storage

--- lib/jobco/job.rb
+++ lib/jobco/job.rb
@@ -4,15 +4,20 @@ require "resque/job_with_status"
 module JobCo
   # forced inheritance : it comes from JobWithStatus.
   # in another dimension, we would have enjoyed fancy automagical "include JobCo::Fu"
+  #
+  # JobCo::Job marshalizes JobCo::Config (an openstruct) to redis at enqueue
+  # and (lazily) loads it back into @jobco at runtime
   class Job < ::Resque::JobWithStatus
     def self.create options = {}
       self.enqueue(self, options)
     end
 
     def self.enqueue(klass, options = {})
+      require "base64"
+
       uuid = ::Resque::Status.create :options => options
       rn = Redis::Namespace.new("jobco", :redis => ::Resque.redis.redis)
-      rn.hset("conf", uuid, JobCo::Config.to_json)
+      rn.hset("conf", uuid, Base64.encode(Marshal.dump(JobCo::Config)))
       ::Resque.enqueue(klass, uuid, options)
       uuid
     end
@@ -20,9 +25,9 @@ module JobCo
     def jobconf
       return @jobconf if @jobconf
 
-      require "json"
+      require "base64"
       rn = Redis::Namespace.new("jobco", :redis => ::Resque.redis.redis)
-      @jobconf = JSON.parse(rn.hget("conf", @uuid), :symbolize_names => true)
+      @jobconf = Marsh.load(Base64.decode(rn.hget("conf", @uuid)))
       rn.hdel("conf", @uuid)
       @jobconf
     end
