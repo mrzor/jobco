0135abfef704de9951dcdf57f49673e0d5eb7002
mrzor <eli@mrzor.com>
2011-12-14 21:08:36 +0100

    'jobco as a library' improvements
    - fixed a glitch in pattern matching for job names
    - remove (process terminating) abort calls, replaced by fail calls
    - cache known jobs classes after they're loaded

--- lib/jobco/jobs.rb
+++ lib/jobco/jobs.rb
@@ -1,16 +1,17 @@
 module JobCo
   class Jobs
     def self.select_job_class pattern
-      c = self.available_jobs.select { |j| j.to_s.downcase.include?(pattern) }
+      c = self.available_jobs.select { |j| j.to_s.downcase.include?(pattern.downcase) }
       if c.size > 1
-        abort "Different jobs match `#{pattern}': #{c.join(', ')}"
+        fail "Different jobs match `#{pattern}': #{c.join(', ')}"
       elsif c.empty?
-        abort "No jobs are matching `#{pattern}'. Use `jobs ls' command."
+        fail "No jobs are matching `#{pattern}'. Use `jobs ls' command."
       end
       c.first
     end
 
     def self.available_jobs
+      return @@available_jobs if defined?(@@available_jobs)
       require "jobco/jobfile"
       Jobfile::evaluate
       jobs = []
@@ -20,15 +21,15 @@ module JobCo
         Dir[File::join(path, "*.rb")].each { |f| require f }
       end
 
-      # 2) explore job package for jobco::job descendants
+      # 2) explore job package for jobco::job inheritors
       Config.job_modules.each do |mod|
-        abort "#{mod} ain't a module" unless mod.is_a?(Module)
+        fail "#{mod} ain't a module" unless mod.is_a?(Module)
         jobs = jobs + mod.constants.map { |c| mod.const_get(c) }.select do |c|
           c.is_a?(Class) && c.ancestors.include?(::JobCo::Job)
         end
       end
 
-      jobs
+      @@available_jobs = jobs
     end
 
     def self.load_available_jobs
