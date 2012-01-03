47317d3b1e771954ee2c2039996e638b24b18306
mrzor <eli@mrzor.com>
2012-01-03 21:00:02 +0100

    lib entry point refactoring : mandatory resque_redis conf

--- lib/jobco.rb
+++ lib/jobco.rb
@@ -13,20 +13,25 @@ end
 # This is what belongs in this file, and it's not much.
 module JobCo
   require "jobco/api"
+  require "jobco/jobs"
   self.extend(JobCo::API)
 
   require "jobco/jobfile"
   def self.boot filename = Jobfile::find
     Jobfile::evaluate filename # populate JobCo::Config
 
-    if Config.redis
-      if Config.redis.is_a?(String)
-        Resque.redis = Config.redis
-      elsif Config.redis.is_a?(Hash)
-        Resque.redis = Redis.new(Config.redis)
+    resque_redis = Config.resque_redis
+    if resque_redis
+      if resque_redis.is_a?(String)
+        Resque.redis = resque_redis
+      elsif resque_redis.is_a?(Hash)
+        Resque.redis = Redis.new(resque_redis)
+      elsif resque_redis == :default
       else
-        fail "this is the redis jobconf you gave me : #{Config.redis}. its unsupported."
+        fail "invalid 'resque_redis' jobconf : `#{resque_redis}`"
       end
+    else
+      fail "Jobfile: `jobconf 'resque_redis'` is mandatory."
     end
 
     # do not load available jobs automatically
